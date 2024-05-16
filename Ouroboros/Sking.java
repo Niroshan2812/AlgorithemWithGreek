2001dfa368118ebdef171e674760d88925375261
Niroshan <niroshan9812@gmail.com>
2024-05-16 19:55:47 +0530

    final

--- Ouroboros/Sking.java
+++ Ouroboros/Sking.java
@@ -2,4 +2,28 @@ package Ouroboros;
 
 public class Sking {
 
+    private String skin;
+
+    public Sking(String initialSking) {
+       this.skin = initialSking;
+    }
+
+    public void grow() {
+       System.out.println("grow\n");
+    }
+
+    public String getSkin() {
+        return skin;
+    }
+
+    public String shed(){
+        
+        return skin.substring(3);
+    }
+
+    public int getSkize(){
+    
+        return skin.length();
+    }
+
 }
--- Ouroboros/ouroboros.java
+++ Ouroboros/ouroboros.java
@@ -3,6 +3,35 @@ package Ouroboros;
 class ouroboros {
     private Sking sking;
     private String core;
- 
-    
+
+    public ouroboros(String initialSking, String core) {
+        this.sking = new Sking(initialSking);
+        this.core = core;
+    }
+
+    public void iterate() {
+        // life: add element
+        sking.grow();
+
+        // deth
+        if (sking.getSkize() > 10) {
+            System.out.println("die");
+            String newSking = sking.shed();
+            newSking += core.substring(0, 3);
+            
+            sking = new Sking(newSking);
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        ouroboros oroboro =  new ouroboros("0","0");
+
+         // nirwana - Let's decide How much life you have to live 
+        // So this i represent the how much life you spend all over the world In this senario I = 10 so he/she have only 10 life to spend 
+        for (int i =0; i<10;i++){
+            oroboro.iterate();
+            System.out.println("Life counting: "+ i);
+        }
+    }
 }
